60ec483e9fe78e0c3e79f817e2646d4a22d00873
Ryoichi Sakamaki <gerggms14@gmail.com>
2025-11-21 00:34:15 +0900

    number fix

--- test.bash
+++ test.bash
@@ -13,19 +13,40 @@ res=0
 out=$(echo 440 | ./tuner)
 [ "${out}" = "A4  2.0" ] || ng "$LINENO"
 
+out=$(echo 27.5 | ./tuner)
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
+out=$(echo 30.8 | ./tuner)
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
+out=$(echo 32.7 | ./tuner)
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
+out=$(echo 3951 | ./tuner)
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
+out=$(echo 4186 | ./tuner)
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
 #異常な動作
 out=$(echo 10 | ./tuner)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
 
 out=$(echo -10 | ./tuner)
 [ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+[ "${out}" = "Out of tuner measurement range" ] || ng "$LINENO"
 
-out=$(echo 50000 | ./tuner)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
+out=$(echo 5000 | ./tuner)
+[ "$?" = 0 ]      || ng "$LINENO"
+#[ "${out}" = "" ] || ng "$LINENO"
+echo $out
 out=$(echo a | ./tuner)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
